1a18106139fe1b441cd3c6f87caf61f91ce8a196
Chris Chatelain <chris.chatelain@digiex.ca>
2025-03-27 16:05:13 -0500

    Begin switching to PID once the bangbang threshold is passed

--- main/tempController.cxx
+++ main/tempController.cxx
@@ -7,6 +7,11 @@
 
 #include "tempController.hxx"
 
+#include "driver/gpio.h"
+#include "driver/ledc.h"
+
+#include <esp_log.h>
+
 const char *TCTAG = "TempController";
 
 TempController::TempController(TempUI *aTempUi, SPIBusManager *aBusManager) : myTempUi(aTempUi), mySpiBusManager(aBusManager), myInstance(nullptr), myThermocoupleQueue(nullptr)
@@ -36,6 +41,11 @@ TempController::TempController(TempUI *aTempUi, SPIBusManager *aBusManager) : my
 
 	xTaskCreate(&thermocoupleTask, "thermocouple_task", 2048, this, 6, NULL);
 	xTaskCreate(&receiverTask, "receiver_task", 2048, this, 6, NULL);
+
+	myLastCurrentTemp = static_cast<double>(myLastResult.thermocouple_c);
+	static int targetTemp = myTempUi->GetTargetTemp();
+	myLastSetTemp = reinterpret_cast<double *>(&targetTemp);
+	myRelayState = new bool(false);
 }
 
 void TempController::thermocoupleTask(void *pvParameter)
@@ -96,4 +106,58 @@ void TempController::receiverTask(void *pvParameter)
 
 		vTaskDelay(1000 / portTICK_PERIOD_MS);
 	}
+}
+
+void TempController::initPID()
+{
+	// Initialize PID controller
+	myAutoPIDRelay = new AutoPIDRelay(&myLastCurrentTemp, &myLastSetTemp, &myRelayState, 2000.0, 30.0, 50.0, 0.1);
+	myAutoPIDRelay->setBangBang(SSR_REDUCED_PWM_UNDER);
+	myAutoPIDRelay->setTimeStep(1000);
+	myAutoPIDRelay->setOutputRange(SSR_OFF_PWM, SSR_FULL_PWM);
+
+	// Initialize SSR
+	initSSR();
+}
+
+void TempController::initSSR()
+{
+	// Configure GPIO 26 as an output for SSR control
+	// Configure LEDC timer for 1Hz PWM frequency
+	ledc_timer_config_t timer_conf = {};
+	timer_conf.speed_mode = LEDC_LOW_SPEED_MODE;
+	timer_conf.timer_num = LEDC_TIMER_0;
+	timer_conf.duty_resolution = LEDC_TIMER_10_BIT; // 10-bit resolution (0-1023)
+	timer_conf.freq_hz = 1;							// 1 Hz frequency
+	ESP_ERROR_CHECK(ledc_timer_config(&timer_conf));
+
+	// Configure LEDC channel
+	ledc_channel_config_t channel_conf = {};
+	channel_conf.gpio_num = SSR_PIN;
+	channel_conf.speed_mode = LEDC_LOW_SPEED_MODE;
+	channel_conf.channel = LEDC_CHANNEL_0;
+	channel_conf.intr_type = LEDC_INTR_DISABLE;
+	channel_conf.timer_sel = LEDC_TIMER_0;
+	channel_conf.duty = 0; // Start with 0% duty cycle
+	channel_conf.hpoint = 0;
+	ESP_ERROR_CHECK(ledc_channel_config(&channel_conf));
+	ESP_LOGI(TCTAG, "GPIO 22 configured as output for SSR control");
+
+	// Ensure the heater is initially off
+	setSSRDutyCycle(SSR_OFF_PWM);
+}
+
+void TempController::setSSRDutyCycle(int duty)
+{
+	// ensure it's in range
+	if (duty < 0)
+	{
+		duty = 0;
+	}
+	else if (duty > SSR_FULL_PWM)
+	{
+		duty = SSR_FULL_PWM;
+	}
+	ledc_set_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_0, duty);
+	ledc_update_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_0);
 }
